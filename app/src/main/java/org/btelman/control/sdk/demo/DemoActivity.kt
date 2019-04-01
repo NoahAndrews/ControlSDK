2627de680daf51bc3f1cd0ef1dbdb489f2b39217
Brendon Telman <b.telman01@gmail.com>
2019-03-31 19:38:55 -0500

    Update plugins. Update build scripts. Implement BitRise integration

--- app/src/main/java/org/btelman/control/sdk/demo/DemoActivity.kt
+++ app/src/main/java/org/btelman/control/sdk/demo/DemoActivity.kt
@@ -1,6 +1,6 @@
 package org.btelman.control.sdk.demo
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class DemoActivity : AppCompatActivity() {
