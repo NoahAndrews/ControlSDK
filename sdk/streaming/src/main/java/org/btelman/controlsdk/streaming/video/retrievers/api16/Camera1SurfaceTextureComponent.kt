39fda1503637d34172cf86c2e51a1225a55bcba8
Brendon Telman <b.telman01@gmail.com>
2019-09-10 22:15:40 -0500

    Add support for camera1 API to use resolutions

--- sdk/streaming/src/main/java/org/btelman/controlsdk/streaming/video/retrievers/api16/Camera1SurfaceTextureComponent.kt
+++ sdk/streaming/src/main/java/org/btelman/controlsdk/streaming/video/retrievers/api16/Camera1SurfaceTextureComponent.kt
@@ -18,6 +18,7 @@ import org.btelman.controlsdk.streaming.video.retrievers.SurfaceTextureVideoRetr
  */
 @Suppress("DEPRECATION")
 class Camera1SurfaceTextureComponent : SurfaceTextureVideoRetriever(), Camera.PreviewCallback{
+    private var supportedPreviewSizes: MutableList<Camera.Size>? = null
     private var r: Rect? = null
     private var camera : Camera? = null
     private var _widthV1 = 0
@@ -51,6 +52,8 @@ class Camera1SurfaceTextureComponent : SurfaceTextureVideoRetriever(), Camera.Pr
 
     override fun setupCamera(streamInfo : StreamInfo?){ //TODO actually use resolution from here?
         val cameraId = streamInfo?.deviceInfo?.getCameraId() ?: 0
+        val cameraWidth = streamInfo?.width ?: 640
+        val cameraHeight = streamInfo?.height ?: 480
         camera ?: run {
             if(cameraId+1 > Camera.getNumberOfCameras())
                 throw Exception("Attempted to open camera $cameraId. Only ${Camera.getNumberOfCameras()} cameras exist! 0 is first camera")
@@ -59,8 +62,16 @@ class Camera1SurfaceTextureComponent : SurfaceTextureVideoRetriever(), Camera.Pr
         }
         camera?.let {
             val p = it.parameters
-            p.setPreviewSize(640, 480)
-            p.setPictureSize(640, 480)
+            supportedPreviewSizes = p.supportedPreviewSizes
+            var supportsSize = false
+            supportedPreviewSizes?.forEach { size ->
+                if(size.height == cameraHeight && size.width == cameraWidth){
+                    supportsSize = true
+                }
+            }
+            if(!supportsSize) throw java.lang.Exception("Camera size " +
+                    "${cameraWidth}x$cameraHeight not supported by this camera!")
+            p.setPreviewSize(cameraWidth, cameraHeight)
             p.setRecordingHint(true)
             it.parameters = p
             it.setPreviewTexture(mStManager?.surfaceTexture)
