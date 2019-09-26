303355d06eeadee06e69e35f366bd1da0cfac330
Brendon Telman <b.telman01@gmail.com>
2019-09-25 22:56:44 -0500

    Fix ffmpeg issue where one thread would get stuck waiting

--- sdk/streaming/src/main/java/org/btelman/controlsdk/streaming/utils/FFmpegUtil.kt
+++ sdk/streaming/src/main/java/org/btelman/controlsdk/streaming/utils/FFmpegUtil.kt
@@ -4,13 +4,24 @@ import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import java.io.PrintStream
-import java.util.concurrent.CountDownLatch
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Utility functions for FFmpeg
  */
 object FFmpegUtil {
+
+    @Volatile
+    private var ffmpegInitializedResult: Deferred<Boolean>? = null
+
+    private val ffmpegInitStarted = AtomicBoolean(false)
 
     /**
      * Kill FFmpeg by sending garbage data to the outputStream since it does not close on its own
@@ -25,21 +36,39 @@ object FFmpegUtil {
         }
     }
 
+    // blocking ffmpeg initialize that will only initialize once,
+    // and block until result on the other threads that called this same function instead of that
+    // one initializing as well
     fun initFFmpegBlocking(ffmpeg: FFmpeg?) : Boolean{
-        val latch = CountDownLatch(1)
-        try {
-            ffmpeg?.loadBinary(object : LoadBinaryResponseHandler() {
-                override fun onFinish() {
-                    super.onFinish()
-                    latch.countDown()
+        var result = false
+        runBlocking {
+            if(!ffmpegInitStarted.getAndSet(true)){
+                ffmpegInitializedResult?: run{
+                    ffmpegInitializedResult = initFFmpeg(ffmpeg)
                 }
-            })
-        } catch (e: FFmpegNotSupportedException) {
-            e.printStackTrace()
-            return false
+            }
+            result = ffmpegInitializedResult?.await()!!
         }
-        latch.await()
-        return true
+        if(!result){
+            throw FFmpegNotSupportedException("FFmpeg not supported on this device")
+        }
+        return result
+    }
+
+    suspend fun initFFmpeg(ffmpeg: FFmpeg?) = GlobalScope.async{
+        val result = suspendCoroutine<Boolean> { cont ->
+            try {
+                ffmpeg?.loadBinary(object : LoadBinaryResponseHandler() {
+                    override fun onFinish() {
+                        super.onFinish()
+                        cont.resume(true)
+                    }
+                })
+            } catch (e: FFmpegNotSupportedException) {
+                cont.resumeWith(Result.success(false))
+            }
+        }
+        result
     }
 
     fun execute(ffmpeg: FFmpeg?, uuid: String, command: String, responseHandler: FFmpegExecuteResponseHandler) {
